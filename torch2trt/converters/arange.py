33a6868fdfe1ea17af987d72cefc900265b1a9bb
Kshitij Srivastava <kshitij.srivastava@uber.com>
2020-08-20 22:43:19 -0400

    fixed default parameter and LINSPACE class

--- torch2trt/converters/arange.py
+++ torch2trt/converters/arange.py
@@ -8,14 +8,13 @@ def convert_arange(ctx):
     start = get_arg(ctx, 'start', pos=0, default=0)
     end  = get_arg(ctx, 'end', pos=1,default=None)
     step  = get_arg(ctx, 'step', pos=2, default=1)
-    dtype  = get_arg(ctx, 'dtype',  pos=4, default=None)
+    dtype  = get_arg(ctx, 'dtype',  pos=4, default=torch.float32)
 
     output = ctx.method_return
     num_steps = math.floor((end - start) / step)
 
-    layer = ctx.network.add_fill(shape=trt.Dims([num_steps]), op=trt.tensorrt.LINSPACE)
-    layer = ctx.network.add_concatenation(inputs=trt_inputs)
-    layer.axis = dim - 1
+    layer = ctx.network.add_fill(shape=trt.Dims([num_steps]), op=trt.FillOperation.LINSPACE)
+    layer.set_output_type(0, torch_dtype_to_trt(dtype))
     output._trt = layer.get_output(0)
 
 class arange(torch.nn.Module):
